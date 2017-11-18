e4932c74b5f7e478dbf14dc087828132aac01ed6
David Diaz <ddiaz914@gmail.com>
2017-11-18 17:23:51 -0600

    Update for production
    Update application for production
    
    Fix heroku deploy
    
    Update server with history mode
    
    Update server port for production

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+const history = require('connect-history-api-fallback')
+// ^ middleware to redirect all URLs to index.html
+
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname))
+
+app.use(staticFileMiddleware)
+app.use(history())
+app.use(staticFileMiddleware)
+// ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/index.html'))
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log( 'Express serving...' )
+})
