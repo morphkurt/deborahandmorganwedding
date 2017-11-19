e78020b3f1746aa112fa63a3a547e66804fbf9af
David Diaz <ddiaz914@gmail.com>
2017-11-19 13:42:48 -0600

    Correct the google maps links

--- src/pages/Wedding.vue
+++ src/pages/Wedding.vue
@@ -3,16 +3,18 @@
     <section class="section">
       <h1 class="title is-1 is-spaced has-text-centered">Ceremony</h1>
       <p class="subtitle has-text-centered">St Francis of Assisi Church</p>
+      <p class="has-text-centered">Begins at 1:00pm</p>
       <p class="has-text-centered">813 W Roosevelt Rd</p>
       <p class="has-text-centered">Chicago, IL 60608</p>
-      <p class="has-text-centered"><a class="button is-info" href="https://www.google.com/maps/dir/''/st+francis+of+assisi+church+chicago/data=!4m5!4m4!1m0!1m2!1m1!1s0x880e2cefeccf2fef:0xbbbceb8435f497b1?sa=X&ved=0ahUKEwjn3Nb-5cjXAhXlhlQKHV63A_wQ9RcIiQEwCw">Directions</a></p>
+      <p class="has-text-centered"><a class="button is-info" href="https://www.google.com/maps/place/St+Francis+of+Assisi+Catholic+Church/@41.8669567,-87.6482355,19z/data=!4m17!1m11!4m10!1m3!2m2!1d-87.6466584!2d41.8716865!1m5!1m1!1s0x880e2cefeccf2fef:0xbbbceb8435f497b1!2m2!1d-87.6476863!2d41.866978!3m4!1s0x880e2cefeccf2fef:0xbbbceb8435f497b1!8m2!3d41.866978!4d-87.6476863">Directions</a></p>
     </section>
     <section class="section">
       <h1 class="title is-1 is-spaced has-text-centered">Reception</h1>
       <p class="subtitle has-text-centered">Palace Regency Banquet Hall</p>
+      <p class="has-text-centered">Begins at 6:00pm</p>
       <p class="has-text-centered">5600 W Cermak Rd</p>
       <p class="has-text-centered">Cicero, IL  60814</p>
-      <p class="has-text-centered"><a class="button is-info" href="https://www.google.com/maps/dir/''/Palace+Regency+Hall+Cicero,+IL+60804/@41.8513139,-87.8342486,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x880e339b075ae08b:0x5634b8b205dd0b4d!2m2!1d-87.7642087!2d41.8513352">Directions</a></p>
+      <p class="has-text-centered"><a class="button is-info" href="https://www.google.com/maps/place/Palace+Regency+Hall/@41.8513139,-87.7653051,18z/data=!4m18!1m12!4m11!1m3!2m2!1d-87.7645541!2d41.8573234!1m6!1m2!1s0x880e339b075ae08b:0x5634b8b205dd0b4d!2spalace+regency+banquet+hall!2m2!1d-87.7642087!2d41.8513352!3m4!1s0x880e339b075ae08b:0x5634b8b205dd0b4d!8m2!3d41.8513352!4d-87.7642087">Directions</a></p>
     </section>
   </div>
 </template>
