77e4bb2048a243a577f3e5d7df48091c9ee201df
David Diaz <ddiaz914@gmail.com>
2017-12-10 14:17:39 -0600

    Add translations in spanish for pictures

--- src/pages/Story.vue
+++ src/pages/Story.vue
@@ -4,18 +4,18 @@
       <div class="tile is-parent">
         <div class="tile is-child box">
           <img src="/src/assets/images/story-3.jpg" alt="">
-          <h1 class="title has-text-centered">Engaged</h1>
-          <h3 class="subtitle has-text-centered">September 2nd, 2017</h3>
+          <h1 class="title has-text-centered"><span v-if='this.$store.getters.get_language == "English"'>Engaged</span><span v-else>Comprometidos</span></h1>
+          <h3 class="subtitle has-text-centered"><span v-if='this.$store.getters.get_language == "English"'>September 2nd, 2017</span><span v-else>2 de Septiembre de 2017</span></h3>
         </div>
       </div>
       <div class="tile is-4 is-vertical is-parent">
         <div class="tile is-child box">
           <img src="/src/assets/images/story-2.jpg" alt="">
-          <p class="subtitle is-6">Came here on our first date.</p>
+          <p class="subtitle is-6"><span v-if='this.$store.getters.get_language == "English"'>Came here on our first date.</span><span v-else>Vinimos aquí en nuestra primera salida.</span></p>
         </div>
         <div class="tile is-child box">
           <img src="/src/assets/images/story-1.jpg" alt="">
-          <p class="subtitle is-6">First picture we ever took together.</p>
+          <p class="subtitle is-6"><span v-if='this.$store.getters.get_language == "English"'>First picture we ever took together.</span><span v-else>La primera foto que tomamos juntos.</span></p>
         </div>
       </div>
     </div>
@@ -26,17 +26,17 @@
           <div class="tile is-parent">
             <article class="tile is-child box">
               <img src="/src/assets/images/story-9.jpg" alt="">
-              <p class="subtitle">Family Trip to Minnesota</p>
+              <p class="subtitle"><span v-if='this.$store.getters.get_language == "English"'>Family Trip to Minnesota.</span><span v-else>Viaje familiar a Minnesota.</span></p>
             </article>
           </div>
           <div class="tile is-parent is-vertical">
             <article class="tile is-child box">
               <img src="/src/assets/images/story-4.jpg" alt="">
-              <p class="subtitle">Beach Day</p>
+              <p class="subtitle"><span v-if='this.$store.getters.get_language == "English"'>Beach Day</span><span v-else>Día de Playa</span></p>
             </article>
             <article class="tile is-child box">
               <img src="/src/assets/images/story-7.jpg" alt="">
-              <p class="subtitle">David being dumb...</p>
+              <p class="subtitle"><span v-if='this.$store.getters.get_language == "English"'>David being dumb.</span><span v-else>David siendo tonto.</span></p>
             </article>
           </div>
         </div>
@@ -45,7 +45,7 @@
         <article class="tile is-child box">
           <div class="content">
             <img src="/src/assets/images/story-10.jpg" alt="">
-            <p class="subtitle">Cayute</p>
+            <p class="subtitle"><span>Cute</span></p>
           </div>
         </article>
       </div>
@@ -55,19 +55,19 @@
       <div class="tile is-parent">
         <article class="tile is-child box">
           <img src="/src/assets/images/story-8.jpg" alt="">
-          <p class="subtitle">Mike's Wedding</p>
+          <p class="subtitle"><span v-if='this.$store.getters.get_language == "English"'>Mike's Wedding</span><span v-else>La Boda de Mike</span></p>
         </article>
       </div>
       <div class="tile is-parent">
         <article class="tile is-child box">
           <img src="/src/assets/images/story-5.jpg" alt="">
-          <p class="subtitle">Drum</p>
+          <p class="subtitle"><span v-if='this.$store.getters.get_language == "English"'>Drum</span><span v-else>Tambor</span></p>
         </article>
       </div>
       <div class="tile is-parent">
         <article class="tile is-child box">
           <img src="/src/assets/images/story-6.jpg" alt="">
-          <p class="subtitle">First Halloween Together</p>
+          <p class="subtitle"><span v-if='this.$store.getters.get_language == "English"'>First Halloween Together</span><span v-else>Primero Halloween Juntos</span></p>
         </article>
       </div>
     </div>
